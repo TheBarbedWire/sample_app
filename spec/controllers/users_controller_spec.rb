de7563b3f4d98da2ba457fd5a3653e3316108351
Andrew Barber <andrew.ab@gmail.com>
2011-11-29 17:12:15 +0000

    User model with passwords, and user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,20 +3,37 @@ require 'spec_helper'
 describe UsersController do
   render_views
   
-  before(:each) do
-    @base_title = "Ruby on Rails Tutorial Sample App | "
-  end
-
-  describe "GET 'new'" do
+  describe "GET 'show'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
     it "should be successful" do
-      get 'new'
+      get :show, :id => @user
       response.should be_success
     end
     
-    it "should have the right title" do
-      get 'new'
-      response.should have_selector("title", :content => @base_title + "Sign up")
+    it "should find right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+    
+    it "should have the user's name" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+    
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
     end
   end
+    
 
 end
